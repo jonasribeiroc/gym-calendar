81ae48824025b238df06d66b54c88e04bf45d027
jonasribeiroc <jonasribeiroc@gmail.com>
2022-11-10 13:30:17 -0300

    feat: add app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const LOCAL_STORAGE_DATA_KEY = "@GymCalendar:data";
 
 function App() {
+  const days = 60;
+  const columns = 10;
+  const localData = localStorage.getItem(LOCAL_STORAGE_DATA_KEY);
+  const [data, setData] = useState([]);
+
+  const getNewData = () => {
+    const arr = [...new Array(days)];
+    return arr.map((d, i) => {
+      return { day: i + 1, done: false };
+    });
+  };
+
+  const handlerClickDay = (day) => {
+    const newData = [...data];
+    newData.find(d => d.day === day).done = !(newData.find(d => d.day === day).done);
+    setData(newData);
+    localStorage.setItem(LOCAL_STORAGE_DATA_KEY, JSON.stringify(newData));
+  }
+
+  useEffect(() => {
+    if (localData) {
+      setData(JSON.parse(localData))
+    } else {
+      const newData = getNewData();
+      setData(newData);
+      localStorage.setItem(LOCAL_STORAGE_DATA_KEY, JSON.stringify(newData));
+    }
+  }, [localData])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Calendar">
+      {data.map((d, i) => (
+        <div key={d.day} style={{ width: `calc(${100/columns}% - 1px)`, }} className={`DayWrapper ${d.done ? "Done" : ""}`} onClick={() => handlerClickDay(d.day)}>
+          <div className="Day">{d.day}</div>
+        </div>
+      ))}
     </div>
   );
 }
